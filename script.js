9044d744f819418f27c26a672a7d3a12c6e7c0e5
Omar Ochoa <omar@ochoawebdesign.com>
2023-08-26 15:16:19 -0700

    Chore: line indenting

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("serviceworker.js").then(registration => {
-        console.log("SW Registered!");
-        console.log(registration);
+    navigator.serviceWorker
+        .register("serviceworker.js")
+        .then(registration => {
+            console.log("SW Registered!");
+            console.log(registration);
     }).catch(error => {
         console.log("SW Registration Failed!");
         console.log(error);
